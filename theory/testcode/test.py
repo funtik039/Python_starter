e7b270554eee5e1dc0e8d1564cf2bfb1c4833e04
Funtik <kucenko039@gmail.com>
2021-02-12 10:41:17 +0200

    test git

--- theory/testcode/test.py
+++ theory/testcode/test.py
@@ -1,16 +1,2 @@
-# Создадим пустой словать Capitals
-Capitals = dict()
-
-# Заполним его несколькими значениями
-Capitals['Russia'] = 'Moscow'
-Capitals['Ukraine'] = 'Kiev'
-Capitals['USA'] = 'Washington'
-
-Countries = ['Russia', 'France', 'USA', 'Ukraine']
-
-for country in Countries:
-    # Для каждой страны из списка проверим, есть ли она в словаре Capitals
-    if country in Capitals:
-        print('Capital of ' + country + ': ' + Capitals[country])
-    else:
-        print('Cant find in Country database: ' + country)
+str = input("Введите слово")
+print(str)
